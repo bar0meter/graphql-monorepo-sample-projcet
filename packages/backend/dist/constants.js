bc5e687e27a3ba7061788e7340171771ab193dc0
ajinkya <>
2020-11-15 21:52:45 +0530

    added dist folder in gitignore

--- packages/backend/dist/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SESSION_NAME = void 0;
-exports.SESSION_NAME = "";
--- packages/backend/dist/redis.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-const ioredis_1 = __importDefault(require("ioredis"));
-const REDIS_URL = (_a = process.env.REDIS_URL) !== null && _a !== void 0 ? _a : "";
-exports.default = new ioredis_1.default(REDIS_URL);
